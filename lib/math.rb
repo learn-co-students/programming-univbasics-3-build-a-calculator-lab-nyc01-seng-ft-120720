50f28e2bacb6bfb3ce5aab1c17019f04441f00d5
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-20 02:49:02 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+sum
 end
 
 def subtraction(num1, num2)
-
+sum_sub = num1 - num2
+sum_sub
 end
 
 def division(num1, num2)
-
+sum_div = num1 / num2
+sum_div
 end
 
 def multiplication(num1, num2)
-
+sum_mul = num1 * num2
 end
 
 def modulo(num1, num2)
-
+summod = num1 % num2
+summod
 end
 
 def square_root(num)
-
+sqsum = Math.sqrt (num)
+sqsum
 end
